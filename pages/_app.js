f94901df66f36917f2dc808221b41ffd796579de
mjsf1234 <mrityunjaysaraf5678@gmail.com>
2022-03-26 19:42:26 +0530

    backend initiat

--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-import '../styles/globals.css'
-import Footer from '../components/footer'
-function MyApp({ Component, pageProps }) {
-  return (<><Component {...pageProps} />
-  <Footer/>
-  </>)
-}
-
-export default MyApp
